3c8166758666aaafa2e21fd1304bee0f3665be88
ahana_bhattacharya@brown.edu <abhatt46@vscode1.oscar.ccv.brown.edu>
2026-04-23 18:05:53 -0400

    cacl entropies

--- lightshed/train.py
+++ lightshed/train.py
@@ -47,11 +47,29 @@ def train(device = "cpu", train_loader, val_loader):
 
     return model, T
 
-def comp_entropy():
-    pass
+def comp_entropy(i):
+    B = i.size(0)           # B is batch size
+    i = i.view(B, -1)       # flatten the image
+
+    x_min = x.min(dim=1, keepdim=True)[0] #normalize
+    x_max = x.max(dim=1, keepdim=True)[0]
+    x = (x - x_min) / (x_max - x_min + 1e-8)
+
+    entropies = []
+    for i in range(B):
+        hist = torch.histc(x[i], bins=256, min=0, max=1)
+        p = hist / torch.sum(hist)
+        ent = -torch.sum(p * torch.log(p + 1e-8))
+        entropies.append(ent)
+
+    return torch.stack(entropies)
+
+
     
 def comp_threshold(model, val_loader):
     pass
+
+
 
 
 
